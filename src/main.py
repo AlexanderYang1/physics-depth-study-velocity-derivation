20e089c8c364f885c864d2611fbdf8b2962fbb8d
Alexander Yang <alexander.z.yang0@gmail.com>
2026-07-19 19:19:39 +1000

    version history

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 from scipy.spatial.transform import Rotation as R
+from scipy.integrate import cumulative_trapezoid
 
 OBJECT = 'A'
 RUN = 1
 
-ACCEL = pd.read_csv("data/Obj-" + OBJECT + "-Runs/" + str(RUN) + "/Accelerometer.csv", index_col='time')
-ORIENT = pd.read_csv("data/Obj-" + OBJECT + "-Runs/" + str(RUN) + "/Orientation.csv", index_col='time')
+PATH = "/Users/alexanderyang/Documents/Main Directory/Programming/physics-depth-study-velocity-derivation/"
+ACCEL = pd.read_csv(PATH + "data/Obj-" + OBJECT + "-Runs/" + str(RUN) + "/Accelerometer.csv", index_col='time')
+ORIENT = pd.read_csv(PATH + "data/Obj-" + OBJECT + "-Runs/" + str(RUN) + "/Orientation.csv", index_col='time')
 
 
 def rotate(vec: np.ndarray, quat: np.ndarray) -> np.ndarray:
@@ -14,15 +16,18 @@ def rotate(vec: np.ndarray, quat: np.ndarray) -> np.ndarray:
     rotated_accel = rotation.apply(vec)
     return rotated_accel
 
-
 def rotate_all():
     row_count = len(ACCEL) # noqa
+    rotated_list = []
+
     for i in range(row_count):
         accel_vec = ACCEL[['x', 'y', 'z']].iloc[i].to_numpy()
         orient_q = ORIENT[['qw', 'qx', 'qy', 'qz']].iloc[i].to_numpy()
-        rotate(accel_vec, orient_q)
+        rotated_accel = rotate(accel_vec, orient_q)
+        rotated_list.append(rotated_accel)
+
+    return np.array(rotated_list)
 
 if __name__ == "__main__":
-    rotate_all()
-
+    print(rotate_all())
 
